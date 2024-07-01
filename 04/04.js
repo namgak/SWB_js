04413a7f5fa31c1d910ae9fc733fc252ba87d987
biwon <biwon1204@naver.com>
2024-07-01 10:29:30 +0900

    주사위

--- 04/04.js
+++ 04/04.js
@@ -1,27 +1,37 @@
-// 1. DOM 생성이 되면 Img, button 요소를 가져오기
-// 2. button 클릭이 되는 경우를 찾아야 함
-// 3. img src 속성을 변경
-//     - 랜덤 수 발생 -> 해당하는 랜덤수의 이미지를 가져오기
+//사용자와 컴퓨터 랜덤수 생성
 
-
-// 1. DOM 생성이 되면 Img, button 요소를 가져오기
+// 1. DOM 생성이 되면 
 document.addEventListener('DOMContentLoaded', ()=>{
     //img, button 요소를 가져오기
-    const img = document.querySelector('#divContent img')
-    const bt = document.querySelector('#divContent button')
+    const imgCom = document.querySelector('#com')
+    const imgUser = document.querySelector('#user')
+    const bts = document.querySelectorAll('#divContent button')
+    const msg = document.querySelector('#msg') ;
+  
+    console.log(bts);
+  
     // 2. button 클릭이 되는 경우를 찾아야 함
-    bt.addEventListener('click', ()=>{
-        //랜덤수 발생 1~6
-        const n = Math.floor(Math.random() *6)+1;
-        console.log(`${n}.png`);
-
-        //3.
-        img.setAttribute('src',`./image/img/${n}.png`);
-    });
-
-});
-
-
-
-
-
+    for(let bt of bts) {
+      bt.addEventListener('click', ()=>{
+        //랜덤수를 발생 1 ~ 6까지
+        const nCom = Math.floor(Math.random() * 6) + 1 ; 
+  
+        //user 숫자 :문자를 숫자로 변환
+        const nUser = parseInt(bt.textContent.charAt(0));
+        console.log('nUser', nUser) 
+  
+        // 3. img src 속성을 변경 
+        imgCom.setAttribute('src', `./image/img/${nCom}.png`) ;
+        imgUser.setAttribute('src', `./image/img/${nUser}.png`) ;
+  
+        //4. 숫자비교
+        if (nCom === nUser) {
+          msg.innerHTML = '<h1 id="msgRed">맞음</h1>';
+        }
+        else {
+          msg.innerHTML = '<h1 id="msgBlue">틀림</h1>'
+        }
+      }) ;
+    }
+  
+  });
---
+++ 04/04copy.js
@@ -0,0 +1,31 @@
+// 1. DOM 생성이 되면 Img, button 요소를 가져오기
+// 2. button 클릭이 되는 경우를 찾아야 함
+// 3. img src 속성을 변경
+//     - 랜덤 수 발생 -> 해당하는 랜덤수의 이미지를 가져오기
+
+
+//사용자와 컴퓨터 랜덤수 생성
+
+
+// 1. DOM 생성이 되면 Img, button 요소를 가져오기
+document.addEventListener('DOMContentLoaded', ()=>{
+    //img, button 요소를 가져오기
+    const img = document.querySelector('#com')
+    const imgUser = document.querySelector('#user')
+    const bt = document.querySelector('#divContent button')
+    // 2. button 클릭이 되는 경우를 찾아야 함
+    bt.addEventListener('click', ()=>{
+        //랜덤수 발생 1~6
+        const n = Math.floor(Math.random() *6)+1;
+        console.log(`${n}.png`);
+
+        //3.
+        img.setAttribute('src',`./image/img/${n}.png`);
+    });
+
+});
+
+
+
+
+
